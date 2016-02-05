0103f0658f03878773d5c28de9b9de99a4d6b231
Andrew Brooks <andrewbrooksct@gmail.com>
2016-02-05 09:52:12 -0500

    making call to broadbandmap.gov api

--- preprocess_data/reverse_geocode_homevisit_data/assign_censusblock_to_disaster_coords2.R
+++ preprocess_data/reverse_geocode_homevisit_data/assign_censusblock_to_disaster_coords2.R
@@ -17,8 +17,8 @@ library('base')
 library('rjson')
 
 # read data from 2009-2014_RedCross_DisasterCases_sample.csv
-#redcross_disaster_cases <- read.csv("2009-2014_RedCross_DisasterCases_sample.csv",header=TRUE)
-redcross_disaster_cases <- read.csv("/Users/abrooks/Google Drive/Red Cross/smokealarm/data/RedCross/redcross_disaster_cases/2009-2014_RedCross_DisasterCases_sample.csv",header=TRUE)
+redcross_disaster_cases <- read.csv("2009-2014_RedCross_DisasterCases_sample.csv",header=TRUE)
+#redcross_disaster_cases <- read.csv("/Users/abrooks/Google Drive/Red Cross/smokealarm/data/RedCross/redcross_disaster_cases/2009-2014_RedCross_DisasterCases_sample.csv",header=TRUE)
 
 # home fire cases
 redcross_homefire_cases <- redcross_disaster_cases[redcross_disaster_cases$event_type_old_categories == "Fire : Multi-Family" | redcross_disaster_cases$event_type_old_categories == "Fire : Single Family" | redcross_disaster_cases$event_type_old_categories == "Fire",]
@@ -29,11 +29,26 @@ redcross_homefire_cases$esri_latitude_x[1:5]
 redcross_homefire_cases$esri_longitude_x[1:5]
 redcross_homefire_cases$esri_state
 
+
+redcross_homefire_cases$census_tract <- c()
 urls <- c()
 # example of reading url 
-for (i in 1:nrow(redcross_homefire_cases)) {
-  urls[i] <- cat(cat(cat("http://www.broadbandmap.gov/broadbandmap/census/tract?latitude=",
-                         redcross_homefire_cases$esri_latitude_x[i],sep=""),
-                         cat("&longitude=",redcross_homefire_cases$esri_longitude_x[i],sep=""),sep=""),"&format=xml",sep="")
-}
-urls[1]
+system.time({
+  for (i in (1:nrow(redcross_homefire_cases))[1:100]) {
+    urls[i] <- paste0("http://www.broadbandmap.gov/broadbandmap/census/tract?latitude=", 
+                      redcross_homefire_cases$esri_latitude_x[i],
+                      "&longitude=",
+                      redcross_homefire_cases$esri_longitude_x[i],
+                      "&format=xml"
+    )
+    
+    doc <-  xmlTreeParse(urls[i])
+    tract <- xmlSApply(xmlRoot(doc)[[1]][[1]], xmlValue)['fips']
+    redcross_homefire_cases$tract[i] <- tract
+    if(i %% 10 == 0) cat(paste0(i, " records completely matched \n"))
+  }
+})
+
+
+
+
